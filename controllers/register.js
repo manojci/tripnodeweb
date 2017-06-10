a6d6c41a8ccd9a85431e5d903cc9703b7294e4ae
mirudhayaraj <mirudhayaraj@paypal.com>
2017-06-11 00:41:55 +0530

    user register servlce is used for registration

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,10 @@
 var UserModel = require('../models/UserModel'),
     hash = require('../pass').hash,
+    Client = require('node-rest-client').Client,
+    PropertiesReader = require('properties-reader'),
     helper = require('../utils/helper');
+var client = new Client();
+var properties = PropertiesReader('./config/dev/application.properties');
 module.exports.registerHandler = function (req, res) {
     var password = req.body.password;
     var username = req.body.username;
@@ -11,16 +15,22 @@ module.exports.registerHandler = function (req, res) {
             username: username,
             salt: salt,
             hash: hash,
-        }).save(function (err, newUser) {
-            if (err) throw err;
-            helper.authenticate(newUser.username, password, function(err, user){
-                if(user){
-                    req.session.regenerate(function(){
-                        req.session.user = user;
-                        req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
-                        res.redirect('/cleartrip');
-                    });
-                }
+        });
+        var args = {
+            data: user,
+            headers: { "Content-Type": "application/json" }
+        };
+        client.get(properties.get('eureka.client.user.save.url') , function (data, response) {
+            client.post(data.toString(), args, function (data, response) {
+                helper.authenticate(data.username, password, function(err, user){
+                    if(user){
+                        req.session.regenerate(function(){
+                            req.session.user = user;
+                            req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
+                            res.redirect('/cleartrip');
+                        });
+                    }
+                });
             });
         });
     });
--- utils/helper.js
+++ utils/helper.js
@@ -2,26 +2,28 @@
 Helper Functions
 */
 var UserModel = require('../models/UserModel'),
+    Client = require('node-rest-client').Client,
+    PropertiesReader = require('properties-reader'),
     hash = require('../pass').hash;
+
+var client = new Client();
+var properties = PropertiesReader('./config/dev/application.properties');
 
 module.exports.authenticate = function (name, pass, fn) {
         if (!module.parent) console.log('authenticating %s:%s', name, pass);
-
-        UserModel.User.findOne({
-            username: name
-        },
-
-        function (err, user) {
-            if (user) {
-                if (err) return fn(new Error('cannot find user'));
-                hash(pass, user.salt, function (err, hash) {
-                    if (err) return fn(err);
-                    if (hash == user.hash) return fn(null, user);
-                    fn(new Error('invalid password'));
-                });
-            } else {
-                return fn(new Error('cannot find user'));
-            }
+        var queryString = "?username=" + name;
+        client.get(properties.get('eureka.client.user.url') , function (data, response) {
+            client.get(data + queryString, function (data, response) {
+                if (data) {
+                    hash(pass, data.salt, function (err, hash) {
+                        if (err) return fn(err);
+                        if (hash == data.hash) return fn(null, data);
+                        fn(new Error('invalid password'));
+                    });
+                } else {
+                    return fn(new Error('cannot find user'));
+                }
+            });
         });
 }
 module.exports.requiredAuthentication = function (req, res, next) {
